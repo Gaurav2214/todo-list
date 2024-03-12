63c8f2085b78f3737561a0c1ab9010b581d387dc
Gaurav Tripathi <ramantripathi22@gmail.com>
2024-03-12 17:28:39 +0530

    add list

---
+++ src/components/Body.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AddList from './AddList'
+import ShowList from './ShowList'
+
+const Body = () => {
+  return (
+    <React.Fragment>
+        <AddList />
+        <ShowList />
+    </React.Fragment>
+  )
+}
+
+export default Body;
